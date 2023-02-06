36868e9c955bee6131a05fe44c15b8589d6333ab
Alaap Joshi <alaapjoshi@gmail.com>
2023-02-06 18:00:49 -0500

    Fixed entire home page and renamed it to activity_main Now works with emulator/real device

--- app/src/main/java/com/example/scouterapp/MainActivity.java
+++ app/src/main/java/com/example/scouterapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.scouterapp;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    EditText inputText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.match_number);
+    }
+    public void updateText(View view) {
+        System.out.println("Button");
     }
 }
---
+++ app/src/main/java/com/example/scouterapp/home.java
@@ -0,0 +1,5 @@
+package com.example.scouterapp;
+
+public class home {
+
+}
