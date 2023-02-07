b83d2d8ec5c73aff1d18c1adc4bd0a8500b5ae0c
Alaap Joshi <alaapjoshi@gmail.com>
2023-02-06 19:30:31 -0500

    Fixed entire home page and renamed it to activity_main Now works with emulator/real device

--- app/src/main/java/com/example/scouterapp/MainActivity.java
+++ app/src/main/java/com/example/scouterapp/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.scouterapp;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
     EditText inputText;
+    String driverStation;
 
 
     @Override
@@ -16,7 +21,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.match_number);
+        //Match Number
+        EditText matchNumber = (EditText) findViewById(R.id.match_number_input);
+        matchNumber.setTextColor(Color.WHITE);//set the red text color
+        String matchNumberOutput = matchNumber.getText().toString();
+
+
+        //Team Number
+        EditText teamNumber = (EditText) findViewById(R.id.team_number_input);
+        teamNumber.setTextColor(Color.WHITE);//set the red text color
+        String teamNumberOutput = teamNumber.getText().toString();
+
+
+
+
     }
     public void updateText(View view) {
         System.out.println("Button");
