dc1ca858b592779333d69768b19a95a52b8aa9b3
Alaap Joshi <alaapjoshi@gmail.com>
2023-02-07 14:58:53 -0500

    Finished 95% of Home page, we can use the resuable components.

--- app/src/main/java/com/example/scouterapp/MainActivity.java
+++ app/src/main/java/com/example/scouterapp/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.cardview.widget.CardView;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -25,8 +27,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // I don't like github + Androdi Studio
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        // I don't like github + Android Studio
 
 
 
