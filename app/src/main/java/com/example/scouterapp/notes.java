9edf02b7457fed7102ddf64fa16ff00f8e596aaf
Aryan <aryansacharya@gmail.com>
2023-02-08 18:21:41 -0500

    stopwatch

--- app/src/main/java/com/example/scouterapp/notes.java
+++ app/src/main/java/com/example/scouterapp/notes.java
@@ -9,6 +9,6 @@ public class notes extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notes2);
+        setContentView(R.layout.activity_notes);
     }
 }
